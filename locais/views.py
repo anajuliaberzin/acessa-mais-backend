888bdcc2a7961e7c0ccd737330e7ab8643d53ff5
regis <regis@example.com>
2026-09-01 21:13:00 -0300

    feat: add local name search

--- locais/views.py
+++ locais/views.py
@@ -8,6 +8,15 @@ class LocalListView(generics.ListAPIView):
     queryset = Local.objects.all()
     serializer_class = LocalSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        nome = self.request.query_params.get("nome")
+
+        if nome:
+            queryset = queryset.filter(nome__icontains=nome)
+
+        return queryset
+
 
 class LocalDetailView(generics.RetrieveAPIView):
     queryset = Local.objects.all()
